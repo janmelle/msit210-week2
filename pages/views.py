ab17200fdaa4c5e652a396bc98d20ae6f3de3a6d
janmelle <janmellepapa@gmail.com>
2025-09-28 15:53:30 +0800

    Added 404 and 500 template, and gallery

--- pages/views.py
+++ pages/views.py
@@ -13,3 +13,13 @@ def about(request):
 
 def hello(request, name):
     return render(request, 'hello.html', {'name': name, 'title': 'hello'})
+
+def gallery(request):
+    images = ['img1.jpg', 'img2.jpg', 'img3.jpg']
+    return render(request, 'gallery.html', {'title': 'gallery', 'images': images})
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
+
+def server_error_view(request):
+    return render(request, '500.html', status=500)
