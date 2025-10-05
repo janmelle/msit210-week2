98ec69d5356a91e1b87d5dbe0e1556f42d4aebcc
janmelle <janmellepapa@gmail.com>
2025-10-05 19:53:44 +0800

    Initial commit with models, validators, inlines, fixtures

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from pages.models import Post, Comment
 # Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','body','created_at',)
+    inlines = [CommentInline]
+    search_fields = ('title', 'body')
+    # list_filter = ('created_at',)
+    ordering = ('title','body', 'created_at')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author','post','created_at',)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, validators=[MinLengthValidator(3)], unique=True)
+    body = models.TextField(validators=[MinLengthValidator(10)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post_id}."
